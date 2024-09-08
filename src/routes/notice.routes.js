df75fb8269dad2ba9279cde4d4db3fd760bd753a
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2024-09-08 16:18:31 +0600

    feat: Update createNotice route and remove unused routes

--- src/routes/notice.routes.js
+++ src/routes/notice.routes.js
@@ -1,18 +1,9 @@
 const express = require("express");
-const {
-	getNoticeById,
-	getNotices,
-	createNotice,
-} = require("../controllers/notice.controller");
+const { createNotice, getNotice } = require("../controllers/notice.controller");
+
 const router = express.Router();
 
-// Route to create a new notice
-router.post("/", createNotice);
-
-// Route to get all notices
-router.get("/", getNotices);
-
-// Route to get a single notice by ID
-router.get("/:id", getNoticeById);
+router.post("/create", createNotice);
+router.get("/", getNotice);
 
 module.exports = router;
